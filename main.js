21cb164504d4098fd37adeaced64ce1afebd5a34
Basit Oketunde <basitoketunde@basits-mbp.home>
2021-11-27 23:14:40 +0000

    re-designed, sperated functions to own file

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-imoport 
+//English to Morse Code function
+import {translateWords} from './morse-translator.js';
+
+const output = document.querySelector(".output");
+const input = document.querySelector("input");
+const translate = document.querySelector(".translate")
+const reset = document.querySelector(".reset")
+
+
+translate.addEventListener("click", () => {
+    output.innerHTML = translateWords(input.value);
+})
+
+reset.addEventListener("click", () => {
+    output.innerHTML = "";
+    input.value = "";
+})
--- morse-translator.js
+++ morse-translator.js
@@ -1,44 +1,3 @@
-//Translate each letter to morse code
-
-export const translator = (message) => {
-    //console.log("message", message)
-    const morseCode = {
-        a: ".-",
-        b: "-...",
-        c: "-.-.",
-        d: "-..",
-        e: ".",
-        f: "..-.",
-        g: "--.",
-        h: "....",
-        i: "..",
-        j: ".---",
-        k: "-.-",
-        l: ".-..",
-        m: "--",
-        n: "-.",
-        o: "---",
-        p: ".--.",
-        q: "--.-",
-        r: ".-.",
-        s: "...",
-        t: "-",
-        u: "..-",
-        v: "...-",
-        w: ".--",
-        x: "-..-",
-        y: "-.--",
-        z: "--..",
-    }
-    return morseCode[message]
-
-    // if (message === "a") return morseCode.a;
-    // if (message === "b") return morseCode.b;
-    // if (message === "a") return morseCode.a;
-    // if (message === "b") return morseCode.b;
-
-}
-
 export const translateWords = (message) => {
     const morseCode = {
         a: ".-",
@@ -68,23 +27,12 @@ export const translateWords = (message) => {
         y: "-.--",
         z: "--..",
     }
-
-    let word = message.split("");
-    console.log(word)
-
+    const word = message.split("");
     const translated = word.map(letter => {
-       
-        letter = morseCode[letter];
+
+        const newLetter = letter.toLowerCase();
+        letter = morseCode[newLetter];
         return letter;
-        //console.log(letter);
-        //console.log(letter.join('/'))
-        
     })
-    return translated.join("/")
-
- 
+    return translated.join("");
 }
-
-
-const input = document.querySelector(".input-box");
-
